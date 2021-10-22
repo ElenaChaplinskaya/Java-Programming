bb34d532a8b752d29e9fc4867f7d3cb2c0593082
ElenaChaplinskaya <lobasina_lm@bk.ru>
2021-10-23 00:14:32 +0300

    done homework 4

---
+++ main/java/by/itAcademy/Chaplinskaya/lesson4/App.java
@@ -0,0 +1,66 @@
+package by.itAcademy.Chaplinskaya.lesson4;
+
+import static by.itAcademy.Chaplinskaya.secondLesson.Application.enterInteger;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.print("Введите целое число: ");
+        int number = enterInteger();
+        System.out.println(getSumNumber(number));
+        simpleNumber(number);
+        System.out.println(evenNumber(number));
+        subsequence();
+        System.out.println(factorial(number));
+    }
+
+    public static int getSumNumber(int num) {
+        int sum = num;
+        if (num >= 1) {
+            for (int i = 1; i < num; i++) {
+                sum += i;
+            }
+        } else {
+            System.out.println("Введенное число должно быть положительным!");
+        }
+        return sum;
+    }
+
+    public static void simpleNumber(int num) {
+        int count = 0;
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count += 1;
+            }
+        }
+        if (count <= 2) {
+            System.out.println("Число простое");
+        } else {
+            System.out.println("Число составное");
+        }
+    }
+
+    public static String evenNumber(int num) {
+        int i = num;
+        String answer = i % 2 == 0 ? "четное" : "нечетное";
+        return answer;
+    }
+
+    public static int subsequence() {
+        int num = 90;
+        for (int i = 90; i >= 0; i = i - 5) {
+            System.out.println(num);
+            num -= 5;
+        }
+        return num;
+    }
+
+    public static int factorial(int num) {
+        int result = 1;
+        if (num == 1 || num == 0) {
+            return result;
+        }
+        result = num * factorial(num - 1);
+        return result;
+    }
+}
