99cd5053c0d881f61bd4424cc5fbcf26269d3035
ElenaChaplinskaya <lobasina_lm@bk.ru>
2021-11-24 14:54:54 +0300

    homework 12

--- main/java/by/itAcademy/Chaplinskaya/lesson12/App.java
+++ main/java/by/itAcademy/Chaplinskaya/lesson12/App.java
@@ -1,52 +1,100 @@
 package by.itAcademy.Chaplinskaya.lesson12;
 
-import com.sun.scenario.effect.SepiaTone;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 
 public class App {
 
     public static void main(String[] args) {
 
-//        Map<String, Pet> pets = new HashMap<>();
-//        pets.put("Shelly", new Dog());
-//        pets.put("Basa", new Cat());
-//        pets.put("Zuza", new Parrot());
-//
-//        printPets(pets);
+        Map<String, Pet> pets = new HashMap<>();
+        pets.put("Shelly", new Dog());
+        pets.put("Basa", new Cat());
+        pets.put("Zuza", new Parrot());
+
+        printPets(pets);
 
         Map<String, String> map = new HashMap<>();
         map.put("Вася", "Иванов");
         map.put("Петя", "Иванов");
         map.put("Витя", "Лешов");
         map.put("Саша", "Петров");
-        map.put("Леша", "Иванов");
+        map.put("Леша", "h");
 
-          }
+        System.out.println(isUnique(map));
 
-//    public static void printPets(Map<String, Pet> pets) {
-//        Set<Map.Entry<String, Pet>> set = pets.entrySet();
-//        for (Map.Entry<String, Pet> entry : set) {
-//            System.out.println("Key: " + entry.getKey());
-//        }
-//    }
+        Random random = new Random();
+        Set<Integer> list1 = new HashSet<>(5);
+        Set<Integer> list2 = new HashSet<>(5);
+        for (int i = 0; i < 6; i++)
+            list1.add(random.nextInt(15));
+        for (int i = 0; i < 6; i++)
+            list2.add(random.nextInt(15));
 
+        Iterator<Integer> itr = list1.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next().toString());
+        }
+        System.out.println();
+        Iterator<Integer> itr1 = list2.iterator();
+        while (itr1.hasNext()) {
+            System.out.println(itr1.next().toString());
+        }
+        System.out.println();
+        System.out.println(countCommon(list1, list2));
 
-//здесь мне нужно пройти по элементам MAP , взять все значения value и посмотреть если value уникальный-то true,
-// если одинак знач 2 и более, то false
-    //если цикл foreach делать, то я не понимаю как мне записать и считать одинак элементы не имея i
-    // если брать обычный for, то getValue не получается.
-    public boolean isUnique(Map<String, String> map){
-        for (Map.Entry<String, String> entry:map.entrySet()) { //Map.Entry<String, String> entry:map.entrySet()  int i=0; i< map.size(); i++
-          String value = entry.getValue();
+        Set<String> set = new HashSet<>(Arrays.asList("aaa", "bb", "c", "dddd", "eeeee"));
 
-            if(value.hashCode(i)==value.hashCode(i+1) ){
-                return true;
-            }
-        }return false;
+        System.out.println(removeEvenLength(set));
     }
+
+    public static void printPets(Map<String, Pet> pets) {
+        Set<Map.Entry<String, Pet>> set = pets.entrySet();
+        for (Map.Entry<String, Pet> entry : set) {
+            System.out.println("Key: " + entry.getKey());
+        }
+    }
+
+    public static boolean isUnique(Map<String, String> map) {
+        int count = 0;
+        Map<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : map.entrySet()) {
+            for (Map.Entry<String, String> pair1 : copy.entrySet()) {
+                if (pair.getValue().equals(pair1.getValue())) {
+                    count++;
+                }
+            }
+        }
+        if (count > map.size()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static int countCommon(Set<Integer> list1, Set<Integer> list2) {
+        int count = 0;
+        for (Integer set1 : list1) {
+            if (list2.contains(set1)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static Set<String> removeEvenLength(Set<String> set) {
+        List<String> str = new ArrayList<>();
+        str.addAll(set);
+        for (int i = 0; i < str.size(); i++) {
+            if (str.get(i).length() % 2 == 0) {
+                str.remove(i);
+            }
+        }
+        return new HashSet(str);
+    }
+
 }
+
+
+
+
 
