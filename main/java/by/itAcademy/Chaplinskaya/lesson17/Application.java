70cc2ece2ed444dcc8533be5db4bf2f2e23dd9d8
ElenaChaplinskaya <lobasina_lm@bk.ru>
2021-12-21 21:08:25 +0300

    fixed homework 17

--- main/java/by/itAcademy/Chaplinskaya/lesson17/Application.java
+++ main/java/by/itAcademy/Chaplinskaya/lesson17/Application.java
@@ -3,9 +3,9 @@ package by.itAcademy.Chaplinskaya.lesson17;
 public class Application {
     public static void main(String[] args) throws InterruptedException {
 
-
+        Object obj = new Object();
         for (int i = 0; i < 10; i++) {
-            new MyThread().start();
+            new MyThread(obj).start();
         }
     }
 }
--- main/java/by/itAcademy/Chaplinskaya/lesson17/MyThread.java
+++ main/java/by/itAcademy/Chaplinskaya/lesson17/MyThread.java
@@ -2,13 +2,21 @@ package by.itAcademy.Chaplinskaya.lesson17;
 
 public class MyThread extends Thread {
 
+    Object obj;
 
-    public synchronized void run() {
-        System.out.println(MyThread.currentThread().getName());
-        try {
-            MyThread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public MyThread(Object obj) {
+        this.obj = obj;
+    }
+
+
+    public void run() {
+        synchronized (obj) {
+            System.out.println(MyThread.currentThread().getName());
+            try {
+                MyThread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
