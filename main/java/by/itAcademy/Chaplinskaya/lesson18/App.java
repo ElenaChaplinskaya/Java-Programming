a26d142c4e282d0a83d93f50901be803c7ac8cf2
ElenaChaplinskaya <lobasina1990@gmail.com>
2022-10-17 21:34:56 +0300

    trainee

--- main/java/by/itAcademy/Chaplinskaya/lesson18/App.java
+++ main/java/by/itAcademy/Chaplinskaya/lesson18/App.java
@@ -3,7 +3,6 @@ package by.itAcademy.Chaplinskaya.lesson18;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
-
 public class App {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
